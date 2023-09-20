d46b91b742b914e8e1316900d4ba864f239c3092
anastas786 <agorbatuk08@gmail.com>
2023-09-20 16:02:13 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('form.feedback-form input[name="email"]');
+const messageInput = document.querySelector('form.feedback-form textarea[name="message"]');
+
+
+
+const saveFormStateThrottled = throttle(function () {
+    const formState = {
+        email: emailInput.value,
+        message: messageInput.value
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+
+feedbackForm.addEventListener('input', saveFormStateThrottled);
+
+function clearFormState() {
+    localStorage.removeItem('feedback-form-state');
+}
+
+feedbackForm.addEventListener('input', saveFormStateThrottled);
+
+
+feedbackForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value
+    };
+    console.log('Текущее значение формы: ', formData);
+
+    clearFormState();
+
+    emailInput.value = '';
+    messageInput.value = '';
+});
+
 
